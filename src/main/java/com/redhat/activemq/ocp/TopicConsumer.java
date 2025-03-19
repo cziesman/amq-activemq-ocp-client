feb4d4709e6ad9048b9c7f35cd5804d35ad84c99
Craig Ziesman <cziesman@redhat.com>
2025-03-19 13:05:14 -0700

    working

--- src/main/java/com/redhat/activemq/ocp/TopicConsumer.java
+++ src/main/java/com/redhat/activemq/ocp/TopicConsumer.java
@@ -10,7 +10,7 @@ public class TopicConsumer {
 
     private static final Logger LOG = LoggerFactory.getLogger(TopicConsumer.class);
 
-    @JmsListener(destination = "${topic.name}", containerFactory = "topicListenerContainerFactory")
+    @JmsListener(destination = "${topic.queue.name}", containerFactory = "topicListenerContainerFactory")
     public void processMsg(String message) {
 
         LOG.info("============= Received: {}", message);
